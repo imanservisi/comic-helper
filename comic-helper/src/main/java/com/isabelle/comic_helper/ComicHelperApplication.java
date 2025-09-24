0ed62c233d78ae1fc22b04369f1b9b1fba9220bc
Isa Manservisi <isabelle.manservisi@inetum.com>
2025-09-24 15:45:16 +0200

    création base

---
+++ comic-helper/src/main/java/com/isabelle/comic_helper/ComicHelperApplication.java
@@ -0,0 +1,13 @@
+package com.isabelle.comic_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicHelperApplication.class, args);
+	}
+
+}
---
+++ comic-helper/src/test/java/com/isabelle/comic_helper/ComicHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isabelle.comic_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
